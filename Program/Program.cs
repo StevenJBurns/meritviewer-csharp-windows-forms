6c11722935953f21ab24a27309e50e096aa75dbc
stevenjburns@msn.com <stevenjburns@msn.com>
2018-01-31 19:46:19 -0700

    Moved Program and UI folders from original repo but probably not necessary

---
+++ Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using SJB.MeritViewer.WindowsForms.UI;
+
+namespace SJB.MeritViewer.WindowsForms.Program
+  {
+  static class Program
+    {
+    /// <summary>The main entry point for the application.</summary>
+    [STAThread]
+    static void Main()
+      {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new FormMain());
+      }
+    }
+  }
---
+++ UI/Form1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SJB.MeritViewer.WindowsForms.UI
+  {
+  public partial class FormMain : Form
+    {
+    public FormMain()
+      {
+      InitializeComponent();
+      }
+    }
+  }
